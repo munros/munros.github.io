431c9a3e37b541f7b757b5f58325f17a8c124264
Graham Miller <graham-miller@users.noreply.github.com>
2018-05-19 19:12:27 +0100

    Created source folder

---
+++ src/Map.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet'
+
+export default class Map extends Component {
+  state = {
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13,
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng]
+    return (
+      <LeafletMap style={{height: 200, width: 200}} center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </span>
+          </Popup>
+        </Marker>
+      </LeafletMap>
+    )
+  }
+}
