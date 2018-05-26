2d490b023b01a0054eec77f0c6d0cf2df80cd44b
Graham Miller <graham-miller@users.noreply.github.com>
2018-05-26 22:14:27 +0100

    Added sidebar

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import { Map as LeafletMap, ScaleControl } from "react-leaflet"
 import LayersControl from "./LayersControl"
+import Sidebar from "./Sidebar"
 
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
@@ -11,32 +12,38 @@ import markerShadow from "leaflet/dist/images/marker-shadow.png";
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: marker2x,
-    iconUrl: marker,
-    shadowUrl: markerShadow
+  iconRetinaUrl: marker2x,
+  iconUrl: marker,
+  shadowUrl: markerShadow
 });
 
 export default class Map extends Component {
-  state = {
-    latitude: 57.301703,
-    longitude: -4.60763,
-    zoom: 7,
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 57.301703,
+      longitude: -4.60763,
+      zoom: 7,
+    };
   }
 
   render() {
     return (
-      <LeafletMap
-        className="full-height"
-        style={{ width: "100%" }}
-        center={[this.state.latitude, this.state.longitude]}
-        zoom={this.state.zoom}
-        attributionControl={false}
-        maxBounds={[[this.state.latitude + 5, this.state.longitude - 10], [this.state.latitude - 5, this.state.longitude + 10]]}
-        minZoom={6}
-        maxZoom={18}>
-        <ScaleControl />
-        <LayersControl position="topright"/>
-      </LeafletMap>
+      <div className="full-height">
+        <Sidebar />
+        <LeafletMap
+          className="sidebar-map full-height"
+          style={{ width: "100%" }}
+          center={[this.state.latitude, this.state.longitude]}
+          zoom={this.state.zoom}
+          attributionControl={false}
+          maxBounds={[[this.state.latitude + 5, this.state.longitude - 10], [this.state.latitude - 5, this.state.longitude + 10]]}
+          minZoom={6}
+          maxZoom={18}>
+          <ScaleControl />
+          <LayersControl position="topright" />
+        </LeafletMap>
+      </div>
     )
   }
 }
