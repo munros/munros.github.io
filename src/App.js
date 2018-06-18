60ab274a821e8ec3c06feac9a7c1d9dade2b4f7e
Graham Miller <graham-miller@users.noreply.github.com>
2018-06-18 13:45:54 +0100

    Added material ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import "./App.css";
 import Map from "./components/Map"
 import Header from "./components/Header"
+import 'typeface-roboto'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#827717'
+    },
+    secondary: {
+      main: '#fbc02d'
+    },
+  },
+  status: {
+    danger: 'deepOrange',
+  },
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="full-height">
-        <Header />
-        <Map className="full-height" />
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="full-height">
+          <Header />
+          <Map className="full-height" />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react"
-
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 export default class Header extends Component {
 
   render() {
     return (
       <div id="header">
-        <h1>Scotland's Mountains</h1>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              Scotland's Mountains
+            </Typography>
+          </Toolbar>
+        </AppBar>      
       </div>
     )
   }
