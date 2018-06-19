ccae8828742aef91ef1d7cc39e655bbc0faff201
Graham Miller <graham-miller@users.noreply.github.com>
2018-06-19 21:38:04 +0100

    Updated logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react"
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
+
+import logo from "../logo/white.svg"
+
 export default class Header extends Component {
 
   render() {
     return (
-      <AppBar style={{ background: 'rgba(130, 119, 23, 0.8)'}}>
+      <AppBar style={{ background: "rgba(130, 119, 23, 0.8)"}}>
         <Toolbar>
-          <Typography variant="title" color="inherit">
-            Scotland's Mountains
+          <Typography variant="title" color="inherit" style={{fontWeight: "normal"}}>
+            Scotland&apos;s <img src={logo} style={{height: "1em"}} />ountains
           </Typography>
         </Toolbar>
       </AppBar>      
